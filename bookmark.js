65510a115d29866188d7d0afeb6d5a7cb2694b41
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2019-08-01 00:37:08 +0700

    Create bookmark.js

---
+++ bookmark.js
@@ -0,0 +1,8 @@
+INIT:
+javascript:MA=["112087","688565","112084","690065","112079","689997","112097","688499","112099","688541","112110"]; i=-1;
+
+AUTO:
+javascript: i++; CL=document.getElementById("ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_ASPxCallbackPanel1_btDirectClassRegister_CD"); DC=document.getElementById("ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_ASPxCallbackPanel1_tbDirectClassRegister_I"); DC.value=MA[i]; CL.click();
+
+SUBMIT:
+javascript:document.getElementById("ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_ASPxCallbackPanel1_btSendRegister_CD").click();
